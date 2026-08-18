44afe9878cc75e93494665162f4277ca5ad7d87d
AlgorithmEncoder <algorithmencoder@gmail.com>
2026-08-18 12:50:16 +0200

    v.1.5.5: Language Settings - Fase 3
    - data for spanish, english and valencian
    - Language Settings finished

--- src/constants.py
+++ src/constants.py
@@ -8,7 +8,7 @@ No game logic should be implemented here.
 from pathlib import Path
 import sys
 
-GAME_VERSION = "v.1.5.4"
+GAME_VERSION = "v.1.5.5"
 AUTHOR_NAME = "Ferran Clausell Sánchez"
 
 # ==========================================================
